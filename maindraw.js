27e8b7b3dfd85305b169bc15f9d4940a095ebdf1
haoran song <haoransong@haorandeMacBook-Pro.local>
2020-01-24 12:06:37 +0800

    phrse1 done

--- maindraw.js
+++ maindraw.js
@@ -1,9 +1,10 @@
 
 var NOTINIT = -100
-var mousex=100,mousey=100,fmousex=NOTINIT,fmousey=NOTINIT,fcmousex=NOTINIT,fcmousey=NOTINIT
-var if_ctrl=false;inbtn_line=false;ifline=false;
+var mousex=100,mousey=100,fmousex=NOTINIT,fmousey=NOTINIT,fcmousex=NOTINIT,fcmousey=NOTINIT,fmmousex=NOTINIT,fmmousey=NOTINIT
+var if_ctrl=false;inbtn_line=false;ifline=false;mouse_in_change_linewidth = false;in_canvas=false
 var paint_method='none',mousestatus='up'
-var paint_blank_r = 20
+var paint_blank_r = 30,line_width = 2,line_width_movex=0,mouse_move_x = 0,mouse_move_y = 0
+var line_width_fx=1255.6+14.4*2
 
 
 function move(ev) {
@@ -11,6 +12,10 @@ function move(ev) {
 	// console.log(ev);
 	mousex = ev.offsetX;
 	mousey = ev.offsetY;
+	draw_canvas_border()
+	in_canvas = if_in_square(mousex,mousey,10,10,1240,670)
+
+	if (mouse_in_change_linewidth) {move_linewidth()}
 
 	if (if_in_square(mousex,mousey,1300,100,1350,150)) {
 		// console.log('hi')
@@ -36,7 +41,7 @@ function move(ev) {
 			// paint_line()
 			break;
 		case 'paint_blank':
-			console.log(fmousex,fmousey)
+			// console.log(fmousex,fmousey)
 			ifin = if_in_square(mousex,mousey,1425-paint_blank_r,680-paint_blank_r,paint_blank_r,paint_blank_r)
 			if (ifin) {break;}
 			if (mousestatus=='down') {
@@ -45,18 +50,21 @@ function move(ev) {
 			}
 			break;
 	}
+	mouse_move_x = mousex-fmousex
+	if (mouse_in_change_linewidth) {line_width_movex+=mouse_move_x}
+	mouse_move_y = mousey-fmousey
 	fmousex = mousex;
-	fmousey = mousey;	
+	fmousey = mousey;
 }
 
 function paint_mouse() {
-	
+	if (!in_canvas) {return;}
 	if (fmousex==NOTINIT) {
 		fmousex = mousex;
 		fmousey = mousey;
 	}
 	if (mousex>=1240||mousex<=10||mousey<=10||mousey>=670||fmousex>=1240||fmousex<=10||fmousey<=10||fmousey>=670){
-		fmousex = NOTINIT
+		fmmousex = NOTINIT
 		return;
 	}
 
@@ -67,13 +75,14 @@ function paint_mouse() {
 	c.moveTo(fmousex,fmousey);
 	c.lineTo(mousex,mousey);
 	c.closePath();
-	c.lineWidth = 2;
+	c.lineWidth = line_width;
 	c.stroke();
 	fmousex = mousex
 	fmousey = mousey
 }
 
 function paint_line() {
+	if (!in_canvas) {return}
 	if (fcmousey==NOTINIT) {fcmousex=mousex;fcmousey=mousey}
 	else {
 		var canvas = document.getElementById('main');
@@ -83,7 +92,7 @@ function paint_line() {
 		c.moveTo(fcmousex,fcmousey);
 		c.lineTo(mousex,mousey);
 		c.closePath();
-		c.lineWidth = 2;
+		c.lineWidth = line_width;
 		c.stroke();
 		fcmousex = NOTINIT
 		fcmousey = NOTINIT
@@ -91,13 +100,14 @@ function paint_line() {
 }
 
 function paint_blank(x,y,type=0) {
-	//1 no border
+	if (!in_canvas){return;}
 	var canvas = document.getElementById('main');
 	var c = canvas.getContext("2d");
 	c.beginPath()
 	c.lineWidth = 2;
 	if (type==1) {
-		c.arc(x,y,paint_blank_r+2,0,2*Math.PI);
+		var r = parseInt(paint_blank_r)+2
+		c.arc(x,y,r,0,2*Math.PI);
 		c.fillStyle = '#dedede';
 		c.fill();
 	}
@@ -108,15 +118,72 @@ function paint_blank(x,y,type=0) {
 
 		c.stroke();
 	}
+	change_lineWidth()
 	
+}
+
+function move_linewidth() {
+	var canvas = document.getElementById('main');
+	var c = canvas.getContext("2d");
+	c.clearRect(1270,180,200,100)
+
+	c.beginPath();
+	c.moveTo(1270,230);
+	c.lineTo(1400,230);
+	c.closePath();
+	c.stroke();
+	var nowx = line_width_fx + line_width_movex
+	// console.log(nowx,line_width_movex)
+	c.fillStyle = 'blue';
+	if (nowx<=1270) {nowx=1270}
+	else if (nowx>=1400) {nowx=1400}
+	
+	c.fillRect(nowx,220,10,20);
+	
+	var now_line_width = Math.round((nowx-1255.6)/14.4)
+	c.font = "20px bold 黑体";
+	c.fillStyle = 'black';
+	c.fillText('lineWidth:'+now_line_width,1280,200);
+	c.font = "15px bold 黑体";
+	c.fillText('1',1270,260);
+	c.fillText('10',1390,260);
+	
+	
+}
+
+function change_lineWidth() {
+	var canvas = document.getElementById('main');
+	var c = canvas.getContext("2d");
+	c.clearRect(1270,180,200,100)
+	c.font = "20px bold 黑体";
+	c.fillStyle = 'black';
+	c.fillText('lineWidth:'+line_width,1280,200);
+	c.font = "15px bold 黑体";
+	c.fillText('1',1270,260);
+	c.fillText('10',1390,260);
+
+	c.beginPath();
+	c.moveTo(1270,230);
+	c.lineTo(1400,230);
+	c.closePath();
+	c.stroke();
+
+	c.fillStyle = 'blue';
+	c.fillRect(1255.6+14.4*line_width,220,10,20)
+}
+
+function change_clear_size() {
+	paint_blank_r = prompt('newsize?')
 }
 
 function mousedown(ev) {
 	mousestatus = 'down';
-	// if (if_in_square(mousex,mousey,10,10,1270,640)){
-	// 	paint_method = 'paint_mouse';	
-	// }
-		if (inbtn_line) {
+	if (if_in_square(mousex,mousey,1255.6+14.4*line_width,220,1265.6+14.4*line_width,240)){
+		// console.log('in');
+		mouse_in_change_linewidth = true
+		line_width_movex = 0
+	}
+	if (inbtn_line) {
 		draw_button_shade(1300,100,'#dddddd')
 	}
 	switch(paint_method){
@@ -131,9 +198,20 @@ function mousedown(ev) {
 
 function mouseup(ev) {
 	mousestatus = 'up';
+	if (mouse_in_change_linewidth) {
+		mouse_in_change_linewidth = false;
+		var nowx = line_width_fx+ line_width_movex
+		if (nowx<=1270) {nowx=1270}
+		else if (nowx>=1400) {nowx=1400}
+		line_width = Math.round((nowx-1255.6)/14.4)
+		// console.log(line_width)
+		line_width_fx = nowx
+		change_lineWidth()
+	}
 	switch(paint_method){
 		case 'paint_blank':
 			paint_blank(mousex,mousey,1)
+			draw_canvas_border()
 			break;
 	}
 	fmousex = NOTINIT;
@@ -159,8 +237,9 @@ function keydown(ev) {
 	case 91://command
 		if_ctrl = true;
 		break;
-	case 67:
+	case 67://c
 		if (if_ctrl==true) {clear_canvas();}
+		else {change_clear_size()}
 		break;
 	}
 }
@@ -203,6 +282,7 @@ function if_in_square(x,y,sx1,sy1,sx2,sy2) {
 function draw_canvas_border() {
 	var canvas = document.getElementById('main');
 	var c = canvas.getContext("2d");
+	c.lineWidth = 2;
 	c.strokeRect(10,10,1230,660);
 }	
 
@@ -248,6 +328,7 @@ function draw_button_shade(x,y,colour='#ffffff') {
 	c.stroke();
 }
 draw_canvas_border()
+change_lineWidth()
 draw_button_line(1300,100)
 
 
